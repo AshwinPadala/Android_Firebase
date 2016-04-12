0387c13bfd39b8dd65282ada3d06c540e199b964
AshwinPadala <ashwin.padala@gmail.com>
2016-04-11 19:47:34 -0500

    Android_Firebase

---
+++ Towing_Firebase/app/src/main/java/com/tme/towing_firebase/Config.java
@@ -0,0 +1,8 @@
+package com.tme.towing_firebase;
+/**
+ * Created by ashwi on 4/11/2016.
+ */
+public class Config {
+
+    public static final String FIREBASE_URL = "https://android-towing.firebaseio.com/";
+}
